d34cac005ee40d39bda801d1ef50d2c7b658c352
Chimamanda16 <Chimamandaunachukwu@gmail.com>
2023-06-04 06:20:24 +0100

    Update to save new posts without HTML tags

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
+const cheerio = require("cheerio");
 const Post = require("../models/post.model");
 
 app.use(express.json());
 
 module.exports.addPost = (req) =>{
-    
     const content = req;
-    console.log (content);
-
+    const $ = cheerio.load(content);
+    const toHtml = $.text();
     const newPost = new Post({
-        Body: content
+        Body: toHtml
     });
     newPost.save();
     console.log("saved");
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,6 +1,7 @@
 //Get request for the main page
 const express = require('express');
 const app =  express();
+const cheerio = require("cheerio");
 const postRouter = express.Router();
 
 const { addPost } = require("../controllers/post.controller");

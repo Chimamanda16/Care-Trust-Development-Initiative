c183be5f300348dbdefb45a833cebb95f7887949
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-05-23 18:17:37 +0100

    Remove dumb animation

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,4 @@
 // Defining constants
-const aboutSect = document.querySelector(".about-section");
-const aboutPara = document.querySelector(".about-para");
 const aboutImg = document.querySelector(".about-img");
 const carouselContent = document.querySelectorAll(".carousel-content")[0];
 const workSect = document.querySelector(".work-section");
@@ -15,22 +13,12 @@ for(i=0; i<carouselContent.children.length; i++){
     carouselContent.children[i].style.animation = "lazyload 2s";
 };
 
-// Adding animation to the about Section
-aboutSect.style.opacity = 0;
-const observerOne = new IntersectionObserver(function(entries, observer){
-    if(entries[0].isIntersecting === true){
-        aboutSect.style.opacity = 1;
-        aboutPara.style.animation = "slideinLeft 1s";
-        aboutImg.style.animation = "slideinRight 1s";
-    };
-}, options);
-
 //Adding animation to the work section
 for(i=0; i<3; i++){
     workImg[i].style.opacity = 0;
 };
 
-const observerTwo = new IntersectionObserver (function(entries, observer){
+const observerOne = new IntersectionObserver (function(entries, observer){
     if(entries[0].isIntersecting === true){
         for(i=0; i<3; i++){
             workImg[i].style.opacity = 1;
@@ -39,5 +27,4 @@ const observerTwo = new IntersectionObserver (function(entries, observer){
     };
 }, options);
 
-observerOne.observe(aboutSect);
-observerTwo.observe(workSect);
+observerOne.observe(workSect);

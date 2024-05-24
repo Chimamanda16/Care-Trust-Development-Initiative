e028c8eaffc44a148a8422e410a20dfc42d671fe
Chimamanda16 <chimamandaunachukwu@gmail.com>
2024-05-24 09:44:03 +0100

    Add about route

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const homeRouter = require("./home.routes");
-const aboutRouter = require("./articles.routes");
+const aboutRouter = require("./about.routes");
+const articleRouter = require("./articles.routes");
 const adminRouter = require("./admin.routes");
 const loginRouter = require("./login.routes");
-const {postRouter} = require("./post.routes");
+const {postRouter, article} = require("./post.routes");
 
 router.use("/", homeRouter);
 router.use("/", aboutRouter);
+router.use("/", articleRouter);
 router.use("/", adminRouter);
 router.use("/", loginRouter);
 router.use("/", postRouter);
